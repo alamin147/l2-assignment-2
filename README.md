69133855b511ed8e3100b4a84a3043e2953db722
alamin.147 <alamin.14780@gmail.com>
2023-11-22 23:29:39 +0600

    user interface created

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# project-template
-Porject template
+Live link: 
---
+++ src/app/modules/User/User.interface.ts
@@ -0,0 +1,29 @@
+export type OrdersArray = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type Username = {
+  firstName: string;
+  lastName: string;
+};
+
+export type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: Username;
+  age: number;
+  email: string;
+  isActive: 'active' | 'inactive';
+  hobbies: Array<string>;
+  address: Address;
+  orders: Array<OrdersArray>;
+};
